d0d7372021da95841992dab7f803bc81181d5a34
Odin_9 <kalandarov9@gmail.com>
2021-12-25 16:31:28 +0300

    created project
    created project

---
+++ HW11_Pomodoro_new/HW11_Pomodoro_new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW11_Pomodoro_new
+//
+//  Created by Yuri Kalandarov on 25.12.2021.
+//  Copyright © 2021 Yuri Kalandarov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
